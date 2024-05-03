3c4a5a238104bdc119901ec3e892032f0695f2d9
Arshia Nayebnazar <53967879+Nazarshia2889@users.noreply.github.com>
2024-05-02 23:34:02 -0700

    Update example.py

--- example.py
+++ example.py
@@ -64,33 +64,3 @@ send_message("8584420958", "verizon", "Subject: ALERT\n\n " + response.text)
 print(response.text)
 
 
-# import google.generativeai as genai
-# import os
-# from framesParser import upload_to_gemini
-
-# genai.configure(api_key="AIzaSyCUAR7qw9UPOay8T8Pg9-ExH0OJSCNU89E")
-
-# video_file_name = "Burglary004_x264.mp4" # CRIME VIDEO
-
-# uploaded_files = upload_to_gemini(genai, video_file_name, full_video=False)
-
-# # Create the prompt.
-# prompt = "Describe this video."
-
-# # Set the model to Gemini 1.5 Pro.
-# model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest")
-
-# # Make GenerateContent request with the structure described above.
-# def make_request(prompt, files):
-#   request = [prompt]
-#   for file in files:
-#     request.append(file.timestamp)
-#     request.append(file.response)
-#   return request
-
-# # Make the LLM request.
-# request = make_request(prompt, uploaded_files)
-# response = model.generate_content(request,
-#                                   request_options={"timeout": 600})
-# print(response.text)
-
